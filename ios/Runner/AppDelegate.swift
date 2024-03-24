10212cc2bb59415b49047337e95f5b4f328ce308
ipanropi <irfan100053@gmail.com>
2024-03-24 13:04:24 -0400

    Resolve ios platform build problem

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,32 +1,26 @@
 import UIKit
 import Flutter
 import awesome_notifications
-import shared_preferences_ios
+import shared_preferences_foundation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+      ) -> Bool {
+          GeneratedPluginRegistrant.register(with: self)
+
+          // This function registers the desired plugins to be used within a notification background action
+          SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
+              SwiftAwesomeNotificationsPlugin.register(
+                with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+              SharedPreferencesPlugin.register(
+                with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
+          }
+
+          return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      }
 }
 
-override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      GeneratedPluginRegistrant.register(with: self)
 
-      // This function registers the desired plugins to be used within a notification background action
-      SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
-          SwiftAwesomeNotificationsPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
-          FLTSharedPreferencesPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
-      }
-
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
